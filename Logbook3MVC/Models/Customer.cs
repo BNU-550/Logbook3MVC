2db5ae126a9e442ae87dd604177c07a55937bd3c
Derek Peacock <derek_peacock@yahoo.com>
2022-11-17 10:29:13 +0000

    Added Customer

---
+++ Logbook3MVC/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Logbook3MVC.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, ]
+        public string Name { get; set; } =  String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public string? PhoneNumber { get; set; } = null;
+    }
+}
