117dcfd1322de117c88db2d7a355557dd98302f4
Derek Peacock <derek_peacock@yahoo.com>
2022-11-17 10:39:42 +0000

    added address

---
+++ Logbook3MVC/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Logbook3MVC.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(20)]
+        public string HouseNumber { get; set; } = string.Empty;
+
+        [Required, StringLength(10)]
+
+        public string PostCode { get; set; } = string.Empty ;
+    }
+}
